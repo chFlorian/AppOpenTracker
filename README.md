d10182c74bca4a8ebf53ae7f997455b611b89b48
Florian Schweizer <post.an.flo@gmail.com>
2021-08-17 17:08:26 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # AppOpenTracker
 
-A description of this package.
+AppOpenTracker provides an observable AppOpenTracker.shared instance that can be used to track
+- the last version that the app was opened in
+- the last build that the app was opened in
+- how often the app was opened
--- Sources/AppOpenTracker/AppOpenTracker.swift
+++ Sources/AppOpenTracker/AppOpenTracker.swift
@@ -1,3 +1,34 @@
-struct AppOpenTracker {
-    var text = "Hello, World!"
+import Foundation
+import SwiftUI
+
+extension Bundle {
+    public var releaseVersionNumber: String? {
+        infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    public var buildVersionNumber: String? {
+        infoDictionary?["CFBundleVersion"] as? String
+    }
+}
+
+public class AppOpenTracker: ObservableObject {
+    public static let shared = AppOpenTracker()
+    
+    public let lastOpenedVersion: String = UserDefaults.standard.string(forKey: "lastOpenedVersion") ?? ""
+    public let lastOpenedBuild: String = UserDefaults.standard.string(forKey: "lastOpenedBuild") ?? ""
+    
+    public let currentVersion: String = Bundle.main.releaseVersionNumber ?? ""
+    public let currentBuild: String = Bundle.main.buildVersionNumber ?? ""
+    
+    @Published public var versionFirstOpened: Bool = false
+    @Published public var countOfAppOpenedBefore: Int = UserDefaults.standard.integer(forKey: "appOpenedCount")
+    
+    private init() {
+        UserDefaults.standard.set(countOfAppOpenedBefore + 1, forKey: "appOpenedCount")
+        
+        if lastOpenedVersion != currentVersion {
+            versionFirstOpened = true
+            UserDefaults.standard.setValue(currentVersion, forKey: "lastOpenedVersion")
+            UserDefaults.standard.setValue(currentBuild, forKey: "lastOpenedBuild")
+        }
+    }
 }
