63625267168fa80440bec9a684588365a0b97060
Florian Schweizer <post.an.flo@gmail.com>
2021-11-28 15:44:13 +0100

    Add mac support

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "AppOpenTracker",
     platforms: [
         .iOS(.v13),
+        .macOS(.v11),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -24,8 +25,5 @@ let package = Package(
         .target(
             name: "AppOpenTracker",
             dependencies: []),
-        .testTarget(
-            name: "AppOpenTrackerTests",
-            dependencies: ["AppOpenTracker"]),
     ]
 )
